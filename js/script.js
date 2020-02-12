0d784fa7acb6f16de16a0545cd74e4ae544bdb7e
Iskanderovulan <iskanderovulan@gmail.com>
2020-02-12 16:31:40 +0600

    newfiles-2

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ $('.nav a').on('click',function (){
     $('.mobile-btn').removeClass('show-mobile-btn');
     $('.nav').removeClass('show-nav')
 });
+
+
+    $(document).ready(function(){
+        $("nav").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+                top = $(id).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
